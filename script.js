19df0939ef4eb4e0b737f7b05d95c245170dc151
KsuDeihina <deihina1996@mail.ru>
2022-07-28 22:28:15 +0500

    JSLesson6

---
+++ script.js
@@ -0,0 +1,17 @@
+let mas = [];
+for(let i = 0; i < Infinity; i++){
+    let a = prompt('Введите команду');
+    let arr = a.split(' ');
+    let b = arr[0];
+    let c = arr[1]; 
+    if(b == 'add,'){
+        mas.push(c);
+    } else if(b == 'del,'){
+        let d = mas[i] == c ? i : '';
+        mas.splice(d, 1)
+    } else if (b == 'stop'){
+        break
+    } 
+    console.log(mas);
+}
+// console.log(mas);
---
+++ script1.js
@@ -0,0 +1,19 @@
+function vowels(str) {
+    console.log(str);
+
+let c = 0;
+let arr = str.split("");
+for(let i = 0; i < arr.length; i++){
+        if(arr[i] == 'а' || arr[i] == 'я' || arr[i] == 'о' || arr[i] == 'ё' || arr[i] == 'у' || arr[i] == 'ю' || arr[i] == 'ы' || arr[i] == 'и' || arr[i] == 'э' || arr[i] == 'е' || arr[i] == 'А' || arr[i] == 'Я' || arr[i] == 'О' || arr[i] == 'Ё' || arr[i] == 'У' || arr[i] == 'Ю' || arr[i] == 'И' || arr[i] == 'Э' || arr[i] == 'Е'){           
+            c = c + 1
+        }}
+    return(c)
+        }
+        console.log(c);
+        
+
+        
+    
+
+
+    
